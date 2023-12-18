d73f85b7a02ab908c2abfbc04461fee2400989e1
NDOUM Saïd <83331010+NdSaid@users.noreply.github.com>
2023-12-18 08:52:47 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,7 +1,15 @@
-function skills() {
-  return {
-    name: "skills",
-    type: "array",
-    of: [{ type: "string" }],
-  };
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkills2 = document.getElementById("memberSkills2");
+    var memberSkills3 = document.getElementById("memberSkills3");
+    if (member.checked == true) {
+        memberSkills.style.display = "block";
+        memberSkills2.style.display = "block";
+        memberSkills3.style.display = "block";
+    } else {
+        memberSkills.style.display = "none";
+        memberSkills2.style.display = "none";
+        memberSkills3.style.display = "none";
+    }
 }
